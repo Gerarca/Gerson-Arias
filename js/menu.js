a4666c63a0e5fb705d8fa33f81b58aff67258bf0
Gerarca <gerarca88@gmail.com>
2021-01-25 13:32:05 -0400

    Version 3

---
+++ js/menu.js
@@ -0,0 +1,26 @@
+$(document).ready(main);
+
+var contador = 1;
+
+function main (){
+	$('.menu_bar').click(function(){
+		// $('nav').toggle(); Forma Sencilla de aparecer y desaparecer
+		
+		if (contador == 1){
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		};
+		
+	}); 
+    $('.menu-link').click(function() {
+        contador = 1;
+        $('nav').animate({  left: '-100%' });
+    });	
+};
